0efa0e0d3fd281807c4e2dc1e2540a8f51da7226
Matthew McLeod <mkmmcleod@gmail.com>
2015-03-28 17:06:53 -0400

    Chrome extension works for pinterest

--- chrome/contentscript.js
+++ chrome/contentscript.js
@@ -1,13 +1,16 @@
 
 $( document ).ready(function() {
 	var url = document.URL;
-	console.log(url);
-	console.log((url).indexOf('quora'))
 	if ( (url).indexOf('quora') != -1){
 		console.log("You are at the quora website");
 		$('.modal_signup_dialog').remove();
 		$('.modal_signup_background').remove();
 		$('.modal_signup_facepile').remove();
+	}
+	else if ((url).indexOf('pinterest') != -1){
+		$('.modalScroller').remove();
+		$('.modalContainer').remove();
+		$('.modalMask').remove();
 	}
 })
 

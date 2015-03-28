f58460ca3324bd84a17dc7b2878aa764647667ba
Mohamed <mmmoussa@uwaterloo.ca>
2015-03-28 16:25:52 -0400

    Implemented standalone jquery file for standards compliance

--- chrome/background.js
+++ chrome/background.js
@@ -1,16 +1,5 @@
 chrome.browserAction.onClicked.addListener(function (tab) { //Fired when User Clicks ICON
-    chrome.tabs.executeScript(tab.id, {
-        "file": "contentscript.js"
-    }, function () { // Execute your code
-        console.log("Script Executed .. "); // Notification on Completion
-    });
+  	chrome.tabs.executeScript(tab.id, {"file": "jquery-2.1.3.min.js"}, function () { 
+    	chrome.tabs.executeScript(tab.id, {"file": "contentscript.js"});
+	});
 });
-/*
-chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-	console.log("background lvl 1");
-  chrome.tabs.sendMessage(tabs[0].id, {greeting: "hello"}, function(response) {
-	  console.log("background lvl 2");
-    console.log(response.farewell);
-  });
-});
-*/

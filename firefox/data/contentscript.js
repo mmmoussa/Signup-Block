b62fa13dba1c802a77b4da80811c1befae87b90a
Mohamed <mmmoussa@uwaterloo.ca>
2015-03-28 20:03:31 -0400

    Implemented choice to remove signup pages manually or automatically in Firefox.

--- firefox/data/contentscript.js
+++ firefox/data/contentscript.js
@@ -1,8 +1,7 @@
 
 $( document ).ready(function() {
 	var url = document.URL;
-	if ( (url).indexOf('quora') != -1){
-		console.log("You are at the quora website");
+	if ((url).indexOf('quora') != -1){
 		$('.modal_signup_dialog').remove();
 		$('.modal_signup_background').remove();
 		$('.modal_signup_facepile').remove();
--- firefox/lib/main.js
+++ firefox/lib/main.js
@@ -1,18 +1,35 @@
 var buttons = require('sdk/ui/button/action');
 var tabs = require("sdk/tabs");
 var self = require("sdk/self");
+var preferences = require("sdk/simple-prefs").prefs;
+
+function onPrefChange(prefName) {
+  console.log("The preference " + prefName + " value has changed!");
+}
+require("sdk/simple-prefs").on("AutoRun", onPrefChange);
+
+require("sdk/tabs").on("ready", runScript);
+
+function runScript(tab) {
+  var auto = require('sdk/simple-prefs').prefs['AutoRun']
+  if (auto == true) {
+    require("sdk/tabs").activeTab.attach({
+      contentScriptFile: [self.data.url("jquery-2.1.3.min.js"), self.data.url("contentscript.js")]
+    });
+  };
+}
 
 var button = buttons.ActionButton({
-  	id: "signup-block",
-  	label: "Signup Block",
-  	icon: {
-  		"16": "./icon-16.png",
-    	"32": "./icon-32.png",
-    	"64": "./icon-64.png"
-    },
-  	onClick: function() {
-    	require("sdk/tabs").activeTab.attach({
-      		contentScriptFile: [self.data.url("jquery-2.1.3.min.js"), self.data.url("contentscript.js")]
-    	});
-  	}
+	id: "signup-block",
+	label: "Signup Block",
+	icon: {
+		"16": "./icon-16.png",
+  	"32": "./icon-32.png",
+  	"64": "./icon-64.png"
+  },
+	onClick: function() {
+  	require("sdk/tabs").activeTab.attach({
+    	contentScriptFile: [self.data.url("jquery-2.1.3.min.js"), self.data.url("contentscript.js")]
+  	});
+	}
 });

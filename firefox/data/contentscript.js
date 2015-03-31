a214be1ee644f006826be97cfa788024e0da62a3
Mohamed <mmmoussa@uwaterloo.ca>
2015-03-30 21:53:04 -0400

    Fixed pinterest scroll problem and added several more websites.

--- firefox/data/contentscript.js
+++ firefox/data/contentscript.js
@@ -8,7 +8,6 @@ $(document).ready(function() {
 	var removeList = function(list){
 		for (var i = 0; i < list.length; i++){
 						removeElement($(itemsToRemove[i]));
-
 		}
 	}
 	
@@ -16,12 +15,12 @@ $(document).ready(function() {
 	if ((url).indexOf('quora') != -1){
 		var itemsToRemove = ['.modal_signup_dialog','.modal_signup_background','.modal_signup_facepile']
 		removeList(itemsToRemove)
-
 	}
 	else if ((url).indexOf('pinterest') != -1){
 		var itemsToRemove = ['.modalScroller','.modalContainer','.modalMask'] 
 		removeList(itemsToRemove)
-		
+		$("body").removeClass("noTouch noScroll")
+
 		var interval = setInterval(function(){
 			if ($('.UnauthBanner')){
 				$('.UnauthBanner').remove();
@@ -40,4 +39,49 @@ $(document).ready(function() {
 		    return $(this).css('z-index') == '99998';
 		}).remove();
 	}
+	else if ((url).indexOf('newyorker') != -1){
+		var itemsToRemove = ['#CM-notification']
+		removeList(itemsToRemove)
+		$('.page-container').css('opacity', function(index) {
+			return index = 1;
+		});
+		$('.page-container').css('-webkit-user-select', function(index) {
+			return index = 'auto';
+		});
+		$('.page-container').css('pointer-events', function(index) {
+			return index = 'auto';
+		});
+		$('.content-wrapper').css('-webkit-user-select', function(index) {
+			return index = 'auto';
+		});
+		$('.content-wrapper').css('pointer-events', function(index) {
+			return index = 'auto';
+		});
+	}
+	else if ((url).indexOf('twitter') != -1){
+		var itemsToRemove = ['.SignupDialog', '.alert-messages', '.modal-overlay']
+		removeList(itemsToRemove)
+	}
+	else if ((url).indexOf('youtube') != -1){
+		var itemsToRemove = ['.yt-dialog-fg', '.yt-dialog-bg']
+		removeList(itemsToRemove)
+		$("body").removeClass("yt-dialog-active")
+	}
+	else if ((url).indexOf('pcmag') != -1){
+		var itemsToRemove = ['.bcx_container']
+		removeList(itemsToRemove)
+	}
+	else if ((url).indexOf('thebetterindia') != -1){
+		var itemsToRemove = ['.addthis_bar']
+		removeList(itemsToRemove)
+	}
+	else if ((url).indexOf('cnet') != -1){
+		var itemsToRemove = ['#flyoutPromo', '.social-bar-wrapper']
+		removeList(itemsToRemove)
+	}
+	else if ((url).indexOf('cnbc') != -1){
+		var itemsToRemove = ['.bcx_container', '#social-tools-panel_gig_containerParent']
+		removeList(itemsToRemove)
+	}
+	//'#bcx_98136_iframe_overlay', '.bcx_onsite_els', 
 })
